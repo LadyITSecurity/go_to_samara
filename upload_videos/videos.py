de31368a44867d9deeb202ad652265f56a9931f5
Arisa <uliannaobr@gmail.com>
2023-05-10 00:28:17 +0400

    try to upload videos in the dictionary

--- upload_videos/videos.py
+++ upload_videos/videos.py
@@ -1,20 +1,36 @@
 import cv2
+import os
 
-video = cv2.VideoCapture('videos\\hello_1.mp4')
+tasks_video = {}
 
-if not video.isOpened():
-    print('error')
+# video = cv2.VideoCapture('videos\\hello_1.mp4')
 
-while True:
-    success, img = video.read()
-    cv2.imshow("Video", img)
+# if not video.isOpened():
+#     print('error')
 
-    if cv2.waitKey(25) & 0xFF == ord('u'):
-          break
+
+for root, dirs, files in os.walk('videos'):
+    for file in files:
+        video = cv2.VideoCapture(file)
+        success, img = video.read()
+        tasks_video[file[:-4]] = img
+
+for key, value in tasks_video.items():
+    print(key, ' -> ', value)
+
+
+# print(tasks_video)
+
+# while True:
+#     success, img = video.read()
+#     cv2.imshow("Video", img)
+#
+#     if cv2.waitKey(25) & 0xFF == ord('u'):
+#           break
 
 # ======================================================================================
 # === Словарь видео.
 # ===
 
 
-tasks_video = {}
+
